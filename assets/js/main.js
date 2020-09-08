640b4062fbeafeb8465e5c65f5906dace5998b07
agilatakishiyev <agil.atakishiyev@pashabank.az>
2020-09-08 09:40:35 +0400

    added carousels, made index.html reponsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,4 +57,68 @@
             // }
         }
     });
+
+    // countdown timer code starts
+    document.addEventListener('DOMContentLoaded', function () {
+        const endDateElement = document.querySelector('.end-date');
+        const endDate = new Date(endDateElement.innerText).getTime();
+        const timerDays = document.querySelector('.timer-days');
+        const timerHours = document.querySelector('.timer-hours');
+        const timerMinutes = document.querySelector('.timer-minutes');
+        const timerSeconds = document.querySelector('.timer-seconds');
+
+        setInterval(
+            function () {
+                const now = new Date().getTime();
+                const t = endDate - now;
+
+                if (t >= 0) {
+                    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+                    let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+                    let secs = Math.floor((t % (1000 * 60)) / 1000);
+
+                    timerDays.innerHTML = days;
+                    timerHours.innerHTML = ("0" + hours).slice(-2);
+                    timerMinutes.innerHTML = ("0" + mins).slice(-2);
+                    timerSeconds.innerHTML = ("0" + secs).slice(-2);
+                }
+            },
+            1000
+        );
+    });
+    // countdown timer code ends
+
+    // viewed products carousel code is here bcs it exists in a lot of pages
+    const viewedProducts = new Flickity('.viewed-products--mobile__carousel', {
+        prevNextButtons: false,
+        pageDots: false,
+        cellAlign: 'left',
+        imagesLoaded: true,
+        lazyLoad: false,
+        wrapAround: true
+    });
+    // viewed products carousel code ends
+
+    // products from a category carousel is here bcs it exists in a lot of pages
+    const producstFromAcategory = new Flickity('.products-from-a-category--mobile__carousel', {
+        prevNextButtons: false,
+        pageDots: false,
+        cellAlign: 'left',
+        imagesLoaded: true,
+        lazyLoad: false,
+        wrapAround: true
+    });
+    // products from a category carousel ends here 
+
+    // Products for mobile carousel view starts here
+    const productsMobileCarousel = new Flickity('.products-carousel--mobile__carousel', {
+        prevNextButtons: false,
+        pageDots: false,
+        cellAlign: 'left',
+        imagesLoaded: true,
+        lazyLoad: false,
+        wrapAround: true
+    });
+    // Products for mobile carouse view 
 })()
